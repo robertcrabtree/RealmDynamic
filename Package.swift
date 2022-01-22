917ba703416e33a4f72a35adbfbe8f3401e59044
Robert Crabtree <robert.crabtree@gmail.com>
2022-01-22 01:41:36 -0800

    Clean up

--- Package.swift
+++ Package.swift
@@ -4,38 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "RealmDynamic",
+    name: "RealmUmbrella",
     platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "RealmDynamic",
+            name: "RealmUmbrella",
             type: .dynamic,
-            targets: ["RealmDynamic"]
+            targets: ["RealmUmbrella"]
         ),
         .library(
-            name: "RealmDynamic.static",
-            targets: ["RealmDynamic"]
+            name: "RealmUmbrella.static",
+            targets: ["RealmUmbrella"]
         ),
     ],
     dependencies: [
-        .package(
-            name: "Realm",
-            url: "https://github.com/realm/realm-cocoa",
-            "10.0.0"..<"11.0.0"
-        )
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", "10.0.0"..<"11.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "RealmDynamic",
-            dependencies: [
-                .product(name: "RealmSwift", package: "Realm")
-            ]
+            name: "RealmUmbrella",
+            dependencies: [.product(name: "RealmSwift", package: "Realm")]
         ),
-        .testTarget(
-            name: "RealmDynamicTests",
-            dependencies: ["RealmDynamic"]),
     ]
 )
